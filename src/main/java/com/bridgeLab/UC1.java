b281ec6751a2a3d7c6a4741dd61ed351cf6f71ae
amruta.vatare <vatareamruta615@gmail.com>
2022-08-16 20:59:42 +0530

    uc1 completed

---
+++ src/main/java/com/bridgeLab/UC1.java
@@ -0,0 +1,55 @@
+package com.bridgeLab;
+
+import java.util.Scanner;
+
+public class UC1 {
+    static int rsPerDay = 100;
+    static int ch = 0;
+    static int totalWinAmount=0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int winCount = 0 ;
+        int lossCount = 0;
+        System.out.println("Press 1 to bet");
+        int n = sc.nextInt();
+        if(n == 1){
+            while (rsPerDay!=0){
+                ch = getRandom();
+                switch (ch){
+                    case 1:
+                        gamblingWin();
+                        winCount++;
+                        break;
+                    case 2:
+                        gamblingLoss();
+                        lossCount++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        System.out.println("Win"+winCount+"$ on this day!");
+
+    }
+
+    private static void gamblingLoss() {
+        rsPerDay--;
+        totalWinAmount+=1;
+        System.out.println("Win $1");
+    }
+
+    private static void gamblingWin() {
+        rsPerDay++;
+        totalWinAmount-=1;
+        System.out.println("Loss $1");
+    }
+
+    private static int getRandom(){
+        int min=1,max=3;
+        double random = Math.random();
+        int hours = (int)(random * (max - min))+min;
+        return hours;
+    }
+}
